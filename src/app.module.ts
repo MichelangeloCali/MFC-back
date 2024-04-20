a56608c4992c7c0db0e492402ec6daef9b4b4e57
michalangelo_cali <michelangelocali@hotmail.com>
2024-04-20 19:23:50 -0300

    chore: nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
@@ -6,7 +7,7 @@ import { HealthFacilityModule } from './health-facility/health-facility.module';
 import { ShiftModule } from './shift/shift.module';
 
 @Module({
-  imports: [HealthFacilityModule, ShiftModule],
+  imports: [HealthFacilityModule, ShiftModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
