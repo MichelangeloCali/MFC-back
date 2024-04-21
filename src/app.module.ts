1cc71424e1dad70b36bf790c8e21e288814fa3a0
michalangelo_cali <michelangelocali@hotmail.com>
2024-04-20 21:32:07 -0300

    refactor: remove unused app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { PrismaService } from './prisma.service';
 import { HealthFacilityModule } from './health-facility/health-facility.module';
 import { ShiftModule } from './shift/shift.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HealthFacilityModule, ShiftModule, ConfigModule.forRoot(), UserModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [
+    HealthFacilityModule,
+    ShiftModule,
+    ConfigModule.forRoot(),
+    UserModule,
+  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
